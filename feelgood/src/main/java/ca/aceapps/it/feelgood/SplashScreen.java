423c63061cbed9fde509e5bfcf8d2de689231597
zhiyuan6966 <allenhua@zoho.com>
2021-10-10 00:17:59 +0800

    update Splash Screen

--- feelgood/src/main/java/ca/aceapps/it/feelgood/SplashScreen.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/SplashScreen.java
@@ -4,13 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreen.this, MainActivity.class));
-        finish();
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        Thread mySplash = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                SplashScreen.this.finish();
+
+            }
+        };
+        mySplash.start();
     }
 }
