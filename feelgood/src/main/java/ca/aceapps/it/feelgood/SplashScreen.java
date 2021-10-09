a9a4a79e76449cd79ef35a025c2697f4e0b1c547
zhiyuan6966 <allenhua@zoho.com>
2021-10-10 00:21:38 +0800

    Change duration

--- feelgood/src/main/java/ca/aceapps/it/feelgood/SplashScreen.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/SplashScreen.java
@@ -20,7 +20,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(2000);
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
