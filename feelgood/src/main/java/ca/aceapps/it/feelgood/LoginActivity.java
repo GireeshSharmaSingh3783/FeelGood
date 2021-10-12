6f55b2586004e416bd28bb245d05e9b7d6cde7e9
BrettKean8642 <n01158642@humbermail.ca>
2021-10-12 18:47:41 -0400

    Sign up activity has been created.

--- feelgood/src/main/java/ca/aceapps/it/feelgood/LoginActivity.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/LoginActivity.java
@@ -2,13 +2,30 @@ package ca.aceapps.it.feelgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = (TextView) findViewById(R.id.btn_signup);
+        register.setOnClickListener(this);
+    }
+
+    public void onClick (View v){
+        switch (v.getId()){
+            case R.id.btn_signup:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+
+        }
     }
 }
--- feelgood/src/main/java/ca/aceapps/it/feelgood/SignUpActivity.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/SignUpActivity.java
@@ -2,13 +2,106 @@ package ca.aceapps.it.feelgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextAge, editTextGender, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.Banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.reguser);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullName);
+        editTextAge = (EditText) findViewById(R.id.age);
+        editTextGender = (EditText) findViewById(R.id.gender);
+        editTextEmail  = (EditText) findViewById(R.id.emailreg);
+        editTextPassword = (EditText) findViewById(R.id.passreg);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Banner:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+
+            case R.id.reguser:
+                registerUser();
+
+        }
+
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+        String gender = editTextGender.getText().toString().trim();
+
+        if(fullName.isEmpty()){
+            editTextFullName.setError("Full name is required");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if(age.isEmpty()){
+            editTextAge.setError("Age is required");
+            editTextAge.requestFocus();
+            return;
+        }
+
+        if(gender.isEmpty()){
+            editTextGender.setError("Please provide your gender");
+            editTextGender.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide a valid email address");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Password should be at least 6 characters long");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+    }
+
 }
