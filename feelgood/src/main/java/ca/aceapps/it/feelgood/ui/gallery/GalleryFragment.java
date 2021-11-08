c10224e97391b41a27ba9038053a2ee0b1a2dab6
GireeshSuperSunny3101 <71040997+GireeshSuperSunny3101@users.noreply.github.com>
2021-11-08 07:59:29 -0500

    designed but not implemented recordJournal fragment

--- feelgood/src/main/java/ca/aceapps/it/feelgood/ui/gallery/GalleryFragment.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/ui/gallery/GalleryFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -25,6 +26,7 @@ public class GalleryFragment extends Fragment {
     private FragmentGalleryBinding binding;
 
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
@@ -32,6 +34,10 @@ public class GalleryFragment extends Fragment {
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        ////sad button functionality
+
 
         final TextView textView = binding.textGallery;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
