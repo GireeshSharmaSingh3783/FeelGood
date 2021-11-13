7a50ab7bbddd96b7ef639deb22c92c8d4156213f
zhiyuan6966 <allenhua@zoho.com>
2021-11-13 23:27:22 +0800

    Add snakbar in home fragment.

--- feelgood/src/main/java/ca/aceapps/it/feelgood/ui/home/HomeFragment.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/ui/home/HomeFragment.java
@@ -5,10 +5,15 @@
 //Supriya N01394695
 package ca.aceapps.it.feelgood.ui.home;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,13 +23,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import ca.aceapps.it.feelgood.MainActivity;
 import ca.aceapps.it.feelgood.R;
 import ca.aceapps.it.feelgood.databinding.FragmentHomeBinding;
+import ca.aceapps.it.feelgood.ui.slideshow.SlideshowFragment;
+import ca.aceapps.it.feelgood.ui.slideshow.SlideshowViewModel;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private ImageButton button1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -41,6 +52,38 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+
+        button1 = root.findViewById(R.id.goolebutton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar.make(v,"Do you want go to Google ?",Snackbar.LENGTH_LONG).setAction("Yes",new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openWeb("https://google.ca");
+
+                    }
+
+                    public void openWeb(String url) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(url));
+                        startActivity(intent);
+                    }
+                });
+                snackbar.setActionTextColor(Color.GREEN);
+                snackbar.setTextColor(Color.YELLOW);
+                snackbar.show();
+
+            }
+        });
+
+
+
+
+
+
         return root;
     }
 
