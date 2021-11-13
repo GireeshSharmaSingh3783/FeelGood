4f31d1ca2849e0debe64d4e26e4e531ee2d3fcd2
zhiyuan6966 <allenhua@zoho.com>
2021-11-14 04:26:21 +0800

    add file, add EditText

--- feelgood/src/main/java/ca/aceapps/it/feelgood/ui/gallery/GalleryFragment.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/ui/gallery/GalleryFragment.java
@@ -5,11 +5,13 @@
 //Supriya N01394695
 package ca.aceapps.it.feelgood.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,12 +23,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import ca.aceapps.it.feelgood.R;
 import ca.aceapps.it.feelgood.databinding.FragmentGalleryBinding;
+import ca.aceapps.it.feelgood.ui.slideshow.SlideshowFragment;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
     private Button happy, sad;
+    EditText say;
+    String tell;
 
 
 
@@ -44,10 +49,15 @@ public class GalleryFragment extends Fragment {
         happy=root.findViewById(R.id.happyButton);
         sad=root.findViewById(R.id.sadButton);
 
+        say=root.findViewById(R.id.editText);
+
         happy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(),"I'm happy", Toast.LENGTH_SHORT).show();
+                tell=say.getText().toString();
+                Intent intent = new Intent(getActivity(), SlideshowFragment.class);
+                intent.putExtra("say1",tell);
             }
         });
         sad.setOnClickListener(new View.OnClickListener() {
