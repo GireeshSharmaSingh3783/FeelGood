ce529066261d59db00fb710e1376c196fb27aeaa
GireeshSuperSunny3101 <71040997+GireeshSuperSunny3101@users.noreply.github.com>
2021-11-13 12:31:22 -0500

    updated user class with getters and setters

--- feelgood/src/main/java/ca/aceapps/it/feelgood/User.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/User.java
@@ -7,17 +7,60 @@
 package ca.aceapps.it.feelgood;
 
 public class User {
-    public String fullName, age, gender, email;
+    public String fullName, age, gender, email, password;
+
 
     public User() {
 
     }
 
-    public User(String fullName, String age, String gender, String email) {
+
+    public User(String fullName, String age, String gender, String email, String password) {
         this.fullName = fullName;
         this.age = age;
         this.gender = gender;
         this.email = email;
+        this.password = password;
 
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
