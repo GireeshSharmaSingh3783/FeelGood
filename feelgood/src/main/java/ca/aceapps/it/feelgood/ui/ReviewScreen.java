c39beeaa35bc2aa6476283a54c352e8e44f22974
supriyaN01 <supkhush@yahoo.com>
2021-11-14 22:02:49 -0500

    Created the review screen

--- feelgood/src/main/java/ca/aceapps/it/feelgood/ui/ReviewScreen.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/ui/ReviewScreen.java
@@ -1,3 +1,8 @@
+//Gireesh Sharma-Singh N01193783
+//Zhiyuan Hua N01406966
+//Eghe Iyobosa N01107171
+//Supriya N01394695
+////////////////////////////////////////
 package ca.aceapps.it.feelgood.ui;
 
 import androidx.annotation.NonNull;
