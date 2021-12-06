538e4c20ddf1751f54b71a6710ad7e5bc583ba18
GireeshSuperSunny3101 <71040997+GireeshSuperSunny3101@users.noreply.github.com>
2021-12-06 02:22:42 -0500

    authenticating user sign-in after checking if fields are filled

--- feelgood/src/main/java/ca/aceapps/it/feelgood/SignUpActivity.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/SignUpActivity.java
@@ -27,8 +27,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUpActivity extends AppCompatActivity{
-
+public class SignUpActivity extends AppCompatActivity {
 
 
     private FirebaseAuth mAuth;
@@ -56,10 +55,10 @@ public class SignUpActivity extends AppCompatActivity{
 
 
         editTextFullName = findViewById(R.id.fullName);
-        editTextAge =  findViewById(R.id.age);
-        editTextGender =  findViewById(R.id.gender);
-        editTextEmail  = findViewById(R.id.emailreg);
-        editTextPassword =  findViewById(R.id.passreg);
+        editTextAge = findViewById(R.id.age);
+        editTextGender = findViewById(R.id.gender);
+        editTextEmail = findViewById(R.id.emailreg);
+        editTextPassword = findViewById(R.id.passreg);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         registerUser = (Button) findViewById(R.id.reguser);
@@ -72,89 +71,76 @@ public class SignUpActivity extends AppCompatActivity{
 
                 //Getting values from text fields
                 String name = editTextFullName.getEditText().getText().toString();
-                String age =editTextAge.getEditText().getText().toString();
-                String phoneNumber= editTextGender.getEditText().getText().toString();
-                String email= editTextEmail.getEditText().getText().toString();
-                String password=editTextPassword.getEditText().getText().toString();
+                String age = editTextAge.getEditText().getText().toString();
+                String phoneNumber = editTextGender.getEditText().getText().toString();
+                String email = editTextEmail.getEditText().getText().toString();
+                String password = editTextPassword.getEditText().getText().toString();
                 // password = makePass()
 
-                if(name.isEmpty()){
+                if (name.isEmpty()) {
                     editTextFullName.setError("Full name is required");
                     editTextFullName.requestFocus();
                     return;
                 }
 
-                if(age.isEmpty()){
+                if (age.isEmpty()) {
                     editTextAge.setError("Age is required");
                     editTextAge.requestFocus();
                     return;
                 }
 
-                if(phoneNumber.isEmpty()){
+                if (phoneNumber.isEmpty()) {
                     editTextGender.setError("Please provide your gender");
                     editTextGender.requestFocus();
                     return;
                 }
 
-                if(email.isEmpty()){
+                if (email.isEmpty()) {
                     editTextEmail.setError("Email is required");
                     editTextEmail.requestFocus();
                     return;
                 }
 
-                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                     editTextEmail.setError("Please provide a valid email address");
                     editTextEmail.requestFocus();
                     return;
                 }
 
-                if(password.isEmpty()){
+                if (password.isEmpty()) {
                     editTextPassword.setError("Password is required");
                     editTextPassword.requestFocus();
                     return;
                 }
 
-                if(password.length() < 6){
+                if (password.length() < 6) {
                     editTextPassword.setError("Password should be at least 6 characters long");
                     editTextPassword.requestFocus();
                     return;
                 }
 
-                User users = new User(name, age, phoneNumber, email, password);
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    User users = new User(name, age, phoneNumber, email, password);
 
-                refDB.child(phoneNumber).setValue(users);
+                                    refDB.child(phoneNumber).setValue(users);
+                                }
+                            }
+                        });
+
+
 
             }
         });
 
 
     }
-
-
-//
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()){
-//            case R.id.Banner:
-//                startActivity(new Intent(this, LoginActivity.class));
-//                break;
-//
-//            case R.id.reguser:
-//                registerUser();
-//
-//        }
-//
-//    }
-//
-//    private void registerUser() {
-//        String email = editTextEmail.getText().toString().trim();
-//        String password = editTextPassword.getText().toString().trim();
-//        String fullName = editTextFullName.getText().toString().trim();
-//        String age = editTextAge.getText().toString().trim();
-//        String gender = editTextGender.getText().toString().trim();
-
-
 }
+
+
 //        progressBar.setVisibility(View.VISIBLE);
 //        mAuth.createUserWithEmailAndPassword(email,password)
 //                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
