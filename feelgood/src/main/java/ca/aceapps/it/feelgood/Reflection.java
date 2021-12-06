d96613dec5e66d3f0f684922c56bbb3648641925
GireeshSuperSunny3101 <71040997+GireeshSuperSunny3101@users.noreply.github.com>
2021-12-06 08:16:13 -0500

    Last button from recording journal goes to mainActivity

--- feelgood/src/main/java/ca/aceapps/it/feelgood/Reflection.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/Reflection.java
@@ -2,13 +2,27 @@ package ca.aceapps.it.feelgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Reflection extends AppCompatActivity {
+
+    Button fini;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflection);
+
+        fini = findViewById(R.id.done);
+        fini.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+        });
     }
 }
