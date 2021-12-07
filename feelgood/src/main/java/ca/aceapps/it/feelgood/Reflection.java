d21c02b640fcca8bfdd7b91b539425f4259a42e5
GireeshSuperSunny3101 <71040997+GireeshSuperSunny3101@users.noreply.github.com>
2021-12-07 00:33:36 -0500

    added inputType="textPassword" attribute to ed_password

--- feelgood/src/main/java/ca/aceapps/it/feelgood/Reflection.java
+++ feelgood/src/main/java/ca/aceapps/it/feelgood/Reflection.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class Reflection extends AppCompatActivity {
 
     Button fini;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
